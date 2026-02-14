4a145c4fbab4bdbfddcc55d33cf3ec4586957772
Anshuman-x49 <pikulnayak460@gmail.com>
2026-02-14 10:50:59 +0530

    feat: initialize snake game project with React and Vite
    - Added package.json for project dependencies and scripts
    - Created main application structure with App, Header, and Board components
    - Implemented basic layout and styling in index.css
    - Configured Vite for development and build processes
    - Included Vite logo SVG in public directory

---
+++ Project-05/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Board from './components/Board'
+
+const App = () => {
+  return (
+    <>
+    <div className='container'>
+      <Header />
+      <Board />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ Project-05/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>high score : 0</h1>
+        <h1>score : 0</h1>
+        <h1>Timer : 0</h1>
+    </div>
+  )
+}
+
+export default Header
