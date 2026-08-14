af389ebcf0999c5fbf5d97456cc2726a4ef923ca
Anshuman-x49 <anshumannayak169@gmail.com>
2026-08-14 18:23:06 +0530

    create axios interceptors with automatic token refresh logic

--- TypeScript/src/config/axiosInstance.tsx
+++ TypeScript/src/config/axiosInstance.tsx
@@ -4,3 +4,23 @@ export const axiosInstance = axios.create({
     baseURL: 'https://team-sync-backend-n78w.onrender.com/api',
     withCredentials: true,
 })
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                await axiosInstance.get("/auth/get-accessToken");
+                return axiosInstance(originalRequest);
+            } catch (refreshError) {
+                window.location.href = "/auth/login";
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+)
